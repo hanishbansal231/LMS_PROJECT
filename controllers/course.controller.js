76667ba39949a23408cabb6d65c31527d0cfcbd3
hanishbansal231 <hanishbansal803@gmail.com>
2023-09-26 23:23:55 +0530

    Course Controller Updates

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,23 +1,40 @@
-import Course from "../models/course.model";
-import AppError from "../utils/error.util"
+import Course from "../models/course.model.js";
+import AppError from "../utils/error.util.js"
 
-const getAllCourses = async (req,res,next) => {
-    try{
+const getAllCourses = async (req, res, next) => {
+    try {
         const courses = await Course.find({}).select('-lectures');
-        if(!courses){
-            return next(new AppError('Courses is not found...',401));
+        if (!courses) {
+            return next(new AppError('Courses is not found...', 401));
         }
         res.status(200).json({
             success: true,
-            message:'All Courses',
+            message: 'All Courses',
             courses,
         })
 
-    }catch(e){
-        return  next(new AppError(e.message,401));
+    } catch (e) {
+        return next(new AppError(e.message, 401));
     }
 }
-const getLecturesByCourseId = async (req,res,next) => {}
+const getLecturesByCourseId = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const course = await Course.findById(id);
+        if(!course){
+            return next(new AppError('Courses is not found...', 401));
+        }
+        res.status(200).json({
+            success: true,
+            message: 'Course lectures fetched successfully...',
+            lectures: course.lectures,
+        })
+
+    } catch (e) {
+        return next(new AppError(e.message, 500));
+    }
+}
 
 export {
     getAllCourses,
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,9 +1,10 @@
-import {Router} from 'express';
-import { getAllCourses, getLecturesByCourseId } from '../controllers/course.controller';
+import { Router } from 'express';
+import { getAllCourses, getLecturesByCourseId } from '../controllers/course.controller.js';
+import { isLoggedIn } from '../middlewares/auth.middleware.js';
 const router = Router();
 
 
-router.get('/',getAllCourses);
-router.get('/:id',getLecturesByCourseId);
+router.route('/').get(getAllCourses);
+router.route('/:id').get(isLoggedIn, getLecturesByCourseId);
 
 export default router;
